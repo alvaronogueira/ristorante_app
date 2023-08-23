596fb5837e66c300e78bdd6a2998e07d3db65227
Álvaro Nogueira <98788990+alvaronogueira@users.noreply.github.com>
2023-08-23 17:15:41 -0300

    update menu

--- lib/menu.dart
+++ lib/menu.dart
@@ -4,37 +4,43 @@ const List<Object> highlitghts = [
     "image": "$_imageInitialPath/chicken-salad.png",
     "name": "Chicken Salad",
     "price": "38.90",
-    "description": "Experimente o maravilhoso chicken salad da casa com temperos inesquecíveis e um sabor de dar água na boca!"
+    "description":
+        "Experimente o maravilhoso chicken salad da casa com temperos inesquecíveis e um sabor de dar água na boca!"
   },
   {
     "image": "$_imageInitialPath/chicken-tender.png",
     "name": "Chicken Tender",
     "price": "29.59",
-    "description": "Mais uma vez um grande sucesso da casa: o chicken tender! Venha conhecer essa iguaria italiana com o famoso queijo de parma."
+    "description":
+        "Mais uma vez um grande sucesso da casa: o chicken tender! Venha conhecer essa iguaria italiana com o famoso queijo de parma."
   },
   {
     "image": "$_imageInitialPath/cold-noodles.png",
     "name": "Cold Noodles",
     "price": "17.50",
-    "description": "Como nada antes visto aqui em nosso restaurante, um novo prato para matar a sua fome e vontade de experimentar algo novo"
+    "description":
+        "Como nada antes visto aqui em nosso restaurante, um novo prato para matar a sua fome e vontade de experimentar algo novo"
   },
   {
     "image": "$_imageInitialPath/hamburger-ink.png",
     "name": "Hamburger Ink",
     "price": "28.50",
-    "description": "Você pediu e nós ouvimos! Nosso hamburger de verão veio passar férias neste inverno aqui com a gente."
+    "description":
+        "Você pediu e nós ouvimos! Nosso hamburger de verão veio passar férias neste inverno aqui com a gente."
   },
   {
     "image": "$_imageInitialPath/lasagna-bolognese.jpg",
     "name": "Lasagna Bolognese",
     "price": "35.00",
-    "description": "Um clássico da culinária italiana para sua mesa. Chame sua família toda para apreciar essa massa macia e cheia de recheio, com aquele gostinho de infância."
+    "description":
+        "A italian gastronomic classic. Call all family to flavor stuffed pasta. Paste of chilhood!"
   },
   {
     "image": "$_imageInitialPath/shrimp-n-rice.png",
     "name": "Shrimp 'n rice",
     "price": "48.50",
-    "description": "O que dizer desse prato que mal conheço e considero pacas? Aquela porção bem grande de arroz com os melhores camarões da região!"
+    "description":
+        "O que dizer desse prato que mal conheço e considero pacas? Aquela porção bem grande de arroz com os melhores camarões da região!"
   }
 ];
 
@@ -79,11 +85,7 @@ const List<Object> comidas = [
     "name": "Steak Veggies",
     "price": "48.00"
   },
-  {
-    "image": "$_imageInitialPath/steak.png",
-    "name": "Steak",
-    "price": "48.00"
-  },
+  {"image": "$_imageInitialPath/steak.png", "name": "Steak", "price": "48.00"},
   {
     "image": "$_imageInitialPath/tapioca-taco.jpg",
     "name": "Tapioca Taco",
@@ -139,7 +141,8 @@ const List<Object> pedido = [
     "image": "$_imageInitialPath/chicken-salad.png",
     "name": "Chicken Salad",
     "price": "38.90",
-    "description": "Experimente o maravilhoso chicken salad da casa com temperos inesquecíveis e um sabor de dar água na boca!"
+    "description":
+        "Taste our wonderful chicken salad with indescribable seasoning flavor mouth-watering!"
   },
   {
     "image": "$_imageInitialPath/penne-au-sugo.png",
